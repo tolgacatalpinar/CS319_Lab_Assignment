04bcf8d08242f64e413ddc19f39250fea4077fce
Tolga Çatalpınar <34005953+tolgacatalpinar@users.noreply.github.com>
2020-04-07 12:17:17 +0300

    Main class created and functionality shown

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Main {
+   public static void main(String [] args){
+
+      ArrayList<Integer> ArrayList1 = new ArrayList<Integer>();
+      ArrayList1.add(100);
+      ArrayList1.add(500);
+      ArrayList1.add(900);
+
+      ArrayList<Integer> ArrayList2 = new ArrayList<Integer>();
+      ArrayList2.add(800);
+      ArrayList2.add(600);
+      ArrayList2.add(300);
+
+      ArrayList<Integer> ArrayList3 = new ArrayList<Integer>();
+      ArrayList3.add(400);
+      ArrayList3.add(700);
+      ArrayList3.add(200);
+
+      ArrayOrganizer organizer = new ArrayOrganizer();
+      ArrayList<Integer> result = organizer.concatAndSort(ArrayList1, ArrayList2, ArrayList3);
+      System.out.println(result);
+   }
+
+}
